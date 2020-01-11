3a03b5f04360576174bdf2d7dbef3e9af9e1fcce
zenius <zenius2050@gmail.com>
2020-01-11 19:03:54 +0530

    typescript basic configuration in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,18 @@ const path = require('path');
 
 module.exports = {
 	entry: './src/app.ts', 
+	module: {
+	    rules: [
+	      {
+	        test: /\.tsx?$/,
+	        use: 'ts-loader',
+	        exclude: /node_modules/,
+	      },
+	    ],
+    },
+	resolve: {
+	   extensions: [ '.tsx', '.ts', '.js' ],
+	},
 	output: {
 		path: path.resolve(__dirname, 'dist'), 
 		filename: 'bundle.js'
